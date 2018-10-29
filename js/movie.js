5ea56bbfa08ec7c016ff67bd040022a4f1752c1d
Uthej Goud <goudtigulla@gmail.com>
2018-10-29 08:02:17 +0530

    New Pages

--- js/movie.js
+++ js/movie.js
@@ -3,6 +3,6 @@ var id = qs.get('id');
 $(document).ready(() => {
     movie = $.getJSON("http://192.168.1.11:3000/theaters?id="+id);
     movie.done((data) => {
-        $("img").attr("src","http://192.168.1.11:3000/"+data[0].image);
+        $(".poster").attr("src","http://192.168.1.11:3000/"+data[0].image);
     });
 });
